100aaf886d5edb1b6478b337eea5ac150c7c6dd3
aq17 <aqneco@gmail.com>
2019-12-19 01:08:19 +0900

    🎉 First Commit!

---
+++ .vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: "水の滴るよう",
+  description: 'Breathtakingly beautiful',
+  config: (md) => {
+    md.options.linkify = true
+  },
+  port: '8080',
+  /* head: [
+    ['link', { rel: 'icon', type: 'image/png', href: '/img/favicon.ico' }]
+  ], */
+  themeConfig: {
+    nav: [
+      { text: "Top", link: '/' },
+      {
+        text: "Contact",
+        items: [
+          { text: "Twitter", link: "https://www.twitter.com/momeemt" },
+        ]
+      }
+    ],
+  },
+}
